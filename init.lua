d31eb3cbc58f67e752221c613af09331c959a46c
lazyBoy112 <sangwww1234@gmail.com>
2023-03-13 00:59:06 +0700

    minimal version for neovim

--- init.lua
+++ init.lua
@@ -1,3 +1,32 @@
-require 'lvs.base'
-require 'lvs.plugins'
-require 'lvs.maps'
+local opt = vim.opt
+local keymap = vim.keymap.set
+local arg = { silent = true, noremap = true }
+vim.cmd("autocmd!")
+
+opt.encoding = 'utf-8'
+opt.smartindent = true
+opt.hlsearch = true
+opt.backup = false
+opt.swapfile = false
+opt.expandtab = true
+opt.smarttab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.wrap = false
+opt.clipboard:append { 'unnamedplus' }
+opt.undofile = true
+opt.number = true
+opt.cmdheight = 0
+
+vim.opt.fillchars = { eob = " " }
+
+keymap('n', 'ss', '<cmd>split<cr>', arg)
+keymap('n', 'sv', '<cmd>vsplit<cr>', arg)
+keymap('n', '<C-h>', '<C-w>h')
+keymap('n', '<C-j>', '<C-w>j')
+keymap('n', '<C-k>', '<C-w>k')
+keymap('n', '<C-l>', '<C-w>l')
+keymap('n', '<C-Right>', '<C-w><')
+keymap('n', '<C-Left>', '<C-w>>')
+keymap('n', '<C-Up>', '<C-w>-')
+keymap('n', '<C-Down>', '<C-w>+')
