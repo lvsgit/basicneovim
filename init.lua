d26322d88d38e9ac2832d27a3eec6c930591ad2e
lazyBoy112 <sangwww1234@gmail.com>
2023-03-12 19:13:37 +0700

    add lsp cmp

--- init.lua
+++ init.lua
@@ -1,25 +1,3 @@
--- init.lua
-local options = {
-  swapfile = false,
-  termguicolor = true,
-  backup = false,
-  clipboard = "unamedplus",
-  fileencoding = "utf-8",
-  mouse = 'a',
-  undofile = true,
-  writebackup = false,
-  expandtab = true,
-  shiftwidth = 2,
-  tabstop = 2,
-  cursorline = true,
-  wrap = false,
-  inccommand = "split",
-  list = false,
-  number = true,
-}
-
-for i, j in pairs(options) do
-  vim.opt[i] = j
-end
-
-vim.opt.fillchars = { eob = " " _}
+require 'lvs.base'
+require 'lvs.plugins'
+require 'lvs.maps'
